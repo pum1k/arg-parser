pum1k/arg-parser

commit d42caa674f374fb3fbc1000a2762ea4aac676ff8

Add handling for unrecognised arguments
- ArgParser now stores all unrecognised command
  line arguments
- these arguments can be accessed by calling its
  method get_unrecognised

argparser.hpp
@@ -54,6 +54,7 @@ class ArgParser
     std::unordered_map<std::string, bool> options_flag;
     std::unordered_map<std::string, std::string> options_string;
     std::unordered_map<std::string, int> options_int;
+    std::vector<std::string> unrecognised;
 
     bool default_flag;
     std::string default_string;
@@ -135,6 +136,14 @@ class ArgParser
      */
     int get_int(std::string name);
 
+    /**
+     * get_unrecognised
+     *
+     * Get all unrecognised command line options.
+     *
+     */
+    const std::vector<std::string> &get_unrecognised();
+
     // TODO: help generator
 };
 
@@ -161,13 +170,7 @@ void ArgParser::set_value(Option opt, std::string value)
     }
 }
 
-ArgParser::ArgParser(const std::vector<Option> &options)
-{
-    this->options_.resize(options.size());
-    std::copy(options.begin(), options.end(), this->options_.begin());
-
-    this->set_defaults(false, "", -1);
-}
+ArgParser::ArgParser(const std::vector<Option> &options) : options_(options) {}
 
 void ArgParser::parse(int argc, const char *argv[], int skip_first_n /* = 1 */)
 {
@@ -211,7 +214,8 @@ void ArgParser::parse(int argc, const char *argv[], int skip_first_n /* = 1 */)
                 }
             }
         }
-        // TODO: add handling for unrecognised arguments
+        this->unrecognised.push_back(argv[i]);
+        continue;
     outer_loop:;
     }
 }
@@ -248,6 +252,11 @@ int ArgParser::get_int(std::string name)
         return this->default_int;
     else
         return it->second;
+}
+
+const std::vector<std::string> &ArgParser::get_unrecognised()
+{
+    return this->unrecognised;
 }
 
 } // namespace argp

commit 9a2f9ae888f26ad7efc604dd3945dcba66714e3e

Add function for printing help
- print_help can output simple table of options identifiers and help
  strings
- Option struct was changed, but it shouldn't require changes to
  existing code (you must add help strings to use the full potential of
  this update)
- fix ArgParser constructor bug introduced in d42caa674f374fb3fbc1000a2762ea4aac676ff8

argparser.hpp
@@ -5,6 +5,7 @@
 #ifndef ARGPARSER_ARGPARSER_HPP_
 #define ARGPARSER_ARGPARSER_HPP_
 
+#include <iomanip>
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
@@ -42,6 +43,7 @@ struct Option
     std::string name;
     std::vector<std::string> identifiers;
     OptionType opt_type;
+    std::string help;
 };
 
 /**
@@ -148,7 +150,16 @@ class ArgParser
      */
     const std::vector<std::string> &get_unrecognised();
 
-    // TODO: help generator
+    /**
+     * print_help
+     *
+     * Output help to a specified output stream.
+     *
+     * os - output stream
+     * min_w - minimal number of characters that will be used to display
+     *   identifier of an option. Used for aligning help strings.
+     */
+    void print_help(std::ostream &os, int min_w = 15) const;
 };
 
 // DEFINITIONS =================================================================
@@ -174,7 +185,10 @@ void ArgParser::set_value(Option opt, std::string value)
     }
 }
 
-ArgParser::ArgParser(const std::vector<Option> &options) : options_(options) {}
+ArgParser::ArgParser(const std::vector<Option> &options) : options_(options)
+{
+    this->set_defaults(false, "", -1);
+}
 
 bool ArgParser::parse(int argc, const char *argv[], int skip_first_n /* = 1 */)
 {
@@ -263,6 +277,25 @@ int ArgParser::get_int(std::string name)
 const std::vector<std::string> &ArgParser::get_unrecognised()
 {
     return this->unrecognised;
+}
+
+void ArgParser::print_help(std::ostream &os, int min_w /* = 15 */) const
+{
+    auto flags = os.flags();
+    os << std::left;
+    for (Option opt : this->options_)
+    {
+        auto it  = opt.identifiers.begin();
+        auto eit = opt.identifiers.end();
+        while (it + 1 != eit)
+        {
+            os << *it << std::endl;
+            ++it;
+        }
+
+        os << std::setw(min_w) << ((*it) + " ") << opt.help << std::endl;
+    }
+    os.flags(flags);
 }
 
 } // namespace argp